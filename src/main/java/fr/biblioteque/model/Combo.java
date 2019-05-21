710fe6d58fde2fd040408c9527fa67215d33f1a4
bilel <bilelh532@gmail.com>
2019-05-21 14:00:23 +0200

    Combo DONE

--- src/main/java/fr/biblioteque/model/Combo.java
+++ src/main/java/fr/biblioteque/model/Combo.java
@@ -1,12 +1,49 @@
 package fr.biblioteque.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table (name="Combo")
 public class Combo {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id ;
 	
+	@Column(name = "ID_LIV",  nullable = false, unique = false)
+	private int idLiv ;
+	
+	@Column(name = "ID_EMP",  nullable = false, unique = false)
+	private int idEmp ;
+	
+	// *****GETTER ET SETTER *****//
+	public int getId() {
+		return id ;
+	}
+		
+	public void setId(int id) {
+		this.id = id ;
+	}
+	
+	public int getIdLiv() {
+		return idLiv ;
+	}
+		
+	public void setIdLiv(int idLiv) {
+		this.idLiv = idLiv ;
+	}
+	
+	public int getIdEmp() {
+		return idEmp ;
+	}
+		
+	public void setIdEmp(int idEmp) {
+		this.idEmp = idEmp ;
+	}
 
 }
